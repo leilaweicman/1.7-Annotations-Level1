b6c63a615ae647c1c2a61f2317466205c4b30a9d
leilaweicman <leilaweic@hotmail.com>
2025-10-01 12:23:29 +0200

    feat(exercise2): add Worker, OnsiteWorker and OnlineWorker base classes

---
+++ src/exercise2/OnsiteWorker.java
@@ -0,0 +1,15 @@
+package exercise2;
+
+public class OnsiteWorker extends Worker {
+
+    private static final double GAS = 50.0;
+
+    public OnsiteWorker(String name, String lastName, double pricePerHour) {
+        super(name, lastName, pricePerHour);
+    }
+
+    @Override
+    public double calculateSalary(int hoursWorked) {
+        return hoursWorked * super.getPricePerHour() + GAS;
+    }
+}
