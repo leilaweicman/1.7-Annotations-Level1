421b6de408e1c64c5338ef770bef1d04ea8d471c
leilaweicman <leilaweic@hotmail.com>
2025-10-01 11:27:29 +0200

    feat(ex1): add OnlineWorker with internet fee in salary calculation

---
+++ src/exercise1/OnlineWorker.java
@@ -0,0 +1,15 @@
+package exercise1;
+
+public class OnlineWorker extends Worker{
+
+    private static final double INTERNET = 30.0;
+
+    public OnlineWorker(String name, String lastName, double pricePerHour) {
+        super(name, lastName, pricePerHour);
+    }
+
+    @Override
+    public double calculateSalary(int hoursWorked) {
+        return hoursWorked * super.getPricePerHour() + INTERNET;
+    }
+}
